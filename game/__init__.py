3e2ebf8f1af59d265bbeb5e576462d6fdf10ed91
ju65230-hash <ju65230@gmail.com>
2026-01-08 16:43:05 +0900

    Delete game directory

--- game/__init__.py
+++
@@ -1,9 +0,0 @@
-from .graphic.render import render_test
-
-VERSION=3.5
-
-def print_version_info():
-    print(f"The version of this game is {VERSION}.")
-
-# 여기에 패키지 초기화 코드 작성
-print("Initializing game...")
